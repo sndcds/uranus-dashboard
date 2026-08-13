993713946c7536a8a25bd1387a4dcf05b9cfb2a3
Roald Christesen <roald@grain.one>
2026-08-13 19:25:51 +0200

    fix missing translation and style

--- src/component/user/UranusOrgTeamInviteActivateView.vue
+++ src/component/user/UranusOrgTeamInviteActivateView.vue
@@ -29,7 +29,7 @@
           {{ t('invite_accept_membership_help_message') }}
         </p>
 
-        <button class="uranus-button" type="button" @click="goToOrgs">
+        <button type="button" class="uranus-button--medium" @click="goToOrgs">
           {{ t('invite_accept_go_to_orgs_cta') }}
         </button>
       </template>
@@ -38,7 +38,7 @@
       <template #error>
         <p class="error-message">{{ errorMessage }}</p>
 
-        <router-link to="/admin/dashboard" class="uranus-button">
+        <router-link to="/admin/dashboard" class="uranus-button--medium">
           {{ t('invite_activate_back_to_login') }}
         </router-link>
       </template>
